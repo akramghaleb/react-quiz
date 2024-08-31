a17ce9c5f3aa92ca8a0738056240403191412223
Akram Ghaleb <46090581+akramghaleb@users.noreply.github.com>
2024-08-31 09:53:41 +0300

    Finishing a Quiz

--- src/App.js
+++ src/App.js
@@ -7,6 +7,7 @@ import StartScreen from "./components/StartScreen";
 import Question from "./components/Question";
 import NextButton from "./components/NextButton";
 import Progress from "./components/Progress";
+import FinishScreen from "./components/FinishScreen";
 const initialState = {
   questions: [],
   // 'loading' , 'error' , 'ready' , 'active' , 'finished'
@@ -14,6 +15,7 @@ const initialState = {
   index: 0,
   answer: null,
   points: 0,
+  highscore: 0
 }
 function reducer(state, action) {
   switch (action.type) {
@@ -49,13 +51,21 @@ function reducer(state, action) {
         index: state.index + 1,
         answer: null,
       }
+    case 'finishQuiz':
+      return {
+        ...state,
+        status: 'finished',
+        highscore: state.points > state.highscore
+          ? state.points
+          : state.highscore
+      }
     default:
       throw new Error(`Invalid action`);
   }
 }
 export default function App() {
   const [state, dispatch] = useReducer(reducer, initialState)
-  const { questions, status, index, answer, points } = state
+  const { questions, status, index, answer, points, highscore } = state
 
   const numQuestions = questions.length
   const maxPossiblePoint = questions.reduce((prev, curr) => prev + curr.points, 0)
@@ -83,9 +93,15 @@ export default function App() {
               answer={answer} />
             <Question question={questions[index]}
               dispatch={dispatch} answer={answer} />
-            <NextButton dispatch={dispatch} answer={answer} />
+            <NextButton dispatch={dispatch} answer={answer}
+              index={index}
+              numQuestions={numQuestions} />
           </>
         }
+        {status === 'finished' &&
+          <FinishScreen points={points}
+            maxPossiblePoint={maxPossiblePoint}
+            highscore={highscore} />}
       </Main>
     </div>
   );
